cf63e0ff91820d1cc4c822c9d5ce472283d16c0e
GaddamS <saikumar.gaddam@proagrica.com>
2024-03-15 14:24:57 +0530

    initial commit for backend

---
+++ task-core/src/main/java/com/taskmanagementcore/TaskCoreApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanagementcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCoreApplication.class, args);
+	}
+
+}
